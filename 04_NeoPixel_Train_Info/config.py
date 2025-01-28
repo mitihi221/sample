c42fa81aaa597ed482ae261c819c3599c6f0c365
mitihi221 <128671498+mitihi221@users.noreply.github.com>
2025-01-28 15:26:50 +0900

    Add files via upload

---
+++ 04_NeoPixel_Train_Info/config.py
@@ -0,0 +1,12 @@
+########## 無線 LAN 設定 ##########
+ssid = 'hogehogefugafuga'
+password = 'Himitsud@y0'
+
+########## IP アドレス設定 ##########
+dhcp = True
+# dhcp = True
+# dhcp = True の場合は以下の ip, mask, gateway, dns を無視します。
+static_ip = 192.168.1.221
+static_mask = 255.255.255.0
+static_gateway = 192.168.1.254
+static_dns = 8.8.8.8
